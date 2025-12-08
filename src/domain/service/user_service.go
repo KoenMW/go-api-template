4ccee17810226e3644de45f6d49110663ccf61d5
KoenMW <koen.wijchers@gmail.com>
2025-12-08 12:04:59 +0100

    fix: refactor user handler to use UserService for user operations

--- src/domain/service/user_service.go
+++ src/domain/service/user_service.go
@@ -39,3 +39,41 @@ func (s *UserService) GetUserByID(id uuid.UUID) (*model.UserDTO, error) {
 		Email: user.Email,
 	}, nil
 }
+
+func (s *UserService) ListUsers(perPage int, page int) ([]model.UserDTO, error) {
+	users, err := s.repo.List(perPage, page)
+	if err != nil {
+		return nil, err
+	}
+	var userDTOs []model.UserDTO
+	for _, user := range users {
+		userDTOs = append(userDTOs, model.UserDTO{
+			ID:    user.ID,
+			Name:  user.Name,
+			Email: user.Email,
+		})
+	}
+
+	return userDTOs, nil
+}
+
+func (s *UserService) UpdateUser(userDTO *model.UserDTO) (*model.UserDTO, error) {
+	user := &model.User{
+		ID:    userDTO.ID,
+		Name:  userDTO.Name,
+		Email: userDTO.Email,
+	}
+	updatedUser, err := s.repo.Update(user)
+	if err != nil {
+		return nil, err
+	}
+	return &model.UserDTO{
+		ID:    updatedUser.ID,
+		Name:  updatedUser.Name,
+		Email: updatedUser.Email,
+	}, nil
+}
+
+func (s *UserService) DeleteUser(id uuid.UUID) (uuid.UUID, error) {
+	return s.repo.Delete(id)
+}
